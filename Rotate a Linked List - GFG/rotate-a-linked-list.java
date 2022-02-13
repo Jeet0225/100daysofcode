745b182820f4ea0e22e7fda3847f9bba50bbb1ea
Jeet0225 <95633934+Jeet0225@users.noreply.github.com>
2022-02-13 21:43:29 +0530

    Added solution - LeetHub

---
+++ Rotate a Linked List - GFG/rotate-a-linked-list.java
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+// } Driver Code Ends
+
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution{
+    //Function to rotate a linked list.
+    public Node rotate(Node head, int k) {
+        // add code here
+            if(head == null || head.next == null || k == 0)
+            return head;
+        
+        Node cur = head;
+        int len = 1;
+        while(cur.next != null){
+            len++;
+            cur = cur.next;
+        }
+        
+        cur.next = head;
+        k = k % len;
+        k = k;
+        
+        while(k-- > 0) cur = cur.next;
+        head = cur.next;
+        cur.next = null;
+        
+        return head;
+    }
+}
