9826fb7c38808546436a3c478b812e03a9ab1eba
Jeet0225 <95633934+Jeet0225@users.noreply.github.com>
2022-06-25 20:50:24 +0530

    Added solution - LeetHub

---
+++ Remove all occurences of duplicates in a linked list - GFG/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,104 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class duplicates
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int N = sc.nextInt();
+			duplicates llist = new duplicates(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.removeAllDuplicates(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }
+    
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* Linked List Node class
+
+class Node
+{
+    int data;
+    Node next;
+}
+    
+*/
+
+class Solution
+{
+    public static Node removeAllDuplicates(Node head)
+    {
+        //code here
+        Node cur = head;
+        Node prev = null;
+        while(cur!=null) {
+            Node end = cur;
+            while(end!=null && end.data==cur.data)
+                end = end.next;
+            if(cur.next!=end) {
+                if(prev==null)
+                    head = end;
+                else
+                    prev.next = end;
+            }
+            else if(cur.next==end) {
+                prev = cur;
+            }
+            cur = end;
+        }
+        return head;
+    }
+   
+}
