d34c9df7f0c264636b6895cb1b0d0db3519f10da
Jeet0225 <95633934+Jeet0225@users.noreply.github.com>
2022-10-29 18:29:34 +0530

    Time: 54 ms (97.26%), Space: 120.5 MB (75.34%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int earliestFullBloom(int[] pt, int[] gt) {
+          int n  = pt.length;
+        
+        int arr[] = new int [100001];
+        
+        for(int i=0; i<n; i++){
+            arr[gt[i]] += pt[i];
+        }
+        
+        int curr =0, ans = 0;
+        
+        for(int i=arr.length-1; i>=1; i--){
+            
+            if (arr[i]==0) continue;
+            
+            curr += arr[i];
+            
+            ans = Math.max(ans, curr+i);
+        }
+        
+        return ans;
+    }
+}
